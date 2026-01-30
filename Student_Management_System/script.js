51be1fb3051c6b0514ae861b624f82a6b998e100
Prateek7071 <tiwariprateek123@gmail.com>
2026-01-30 21:41:23 +0530

    resolved a bug where 2nd table renders when trying to search after sorting with gender.

--- Student_Management_System/script.js
+++ Student_Management_System/script.js
@@ -69,6 +69,41 @@ async function renderData(data){
   })
 }
 
+function spawnNewTable(data){
+  // ik this function is redundant by i'm lazy
+  let table2= document.createElement('table')
+  table2.classList.add('student')
+  table2.id = 'female-table';
+  table2.innerHTML=` <tr>
+      <th>ID</th>
+      <th>Name</th>
+      <th>Gender</th>
+      <th>Class</th>
+      <th>Marks</th>
+      <th>Passing</th>
+      <th>Email</th>
+  </tr>`
+  data.forEach(e=>{
+    let name = `${e.first_name} ${e.last_name}`
+    let status = passing(e.passing)
+    table2.innerHTML+=`<tr>
+        <td>${e.id}</td>
+        <td class="name-td">
+          <div class="name-cell">
+            <img src="${e.img_src}" height="38px" width="38px" alt="${name}">
+            <span>${name}</span>
+          </div>
+        </td>
+        <td>${e.gender}</td>
+        <td>${e.class}</td>
+        <td>${e.marks}</td>
+        <td>${status}</td>
+        <td>${e.email}</td>
+    </tr>`
+  })
+  page.appendChild(table2)
+}
+
 btnSortAZ.addEventListener('click',()=>{
   let newData = structuredClone(data);
   newData = newData.map(e=>({
@@ -121,39 +156,6 @@ btnSortGender.addEventListener('click',()=>{
   spawnNewTable(femaleData)
 })
 
-function spawnNewTable(data){
-  // ik this function is redundant by i'm lazy
-  let table2= document.createElement('table')
-  table2.classList.add('student')
-  table2.innerHTML=` <tr>
-      <th>ID</th>
-      <th>Name</th>
-      <th>Gender</th>
-      <th>Class</th>
-      <th>Marks</th>
-      <th>Passing</th>
-      <th>Email</th>
-  </tr>`
-  data.forEach(e=>{
-    let name = `${e.first_name} ${e.last_name}`
-    let status = passing(e.passing)
-    table2.innerHTML+=`<tr>
-        <td>${e.id}</td>
-        <td class="name-td">
-          <div class="name-cell">
-            <img src="${e.img_src}" height="38px" width="38px" alt="${name}">
-            <span>${name}</span>
-          </div>
-        </td>
-        <td>${e.gender}</td>
-        <td>${e.class}</td>
-        <td>${e.marks}</td>
-        <td>${status}</td>
-        <td>${e.email}</td>
-    </tr>`
-  })
-  page.appendChild(table2)
-}
 searchBtn.addEventListener('click',e=>{
   e.preventDefault()
   handleSeach()
@@ -163,7 +165,15 @@ searchInput.addEventListener('input', e=>{
   handleSeach()
 });
 
+function clearExtraTables() {
+  const extraTable = document.getElementById('female-table');
+  if (extraTable) {
+    extraTable.remove();
+  }
+}
+
 function handleSeach(){
+  clearExtraTables();
   const query= searchInput.value.toLowerCase()
   
   const filterData = data.filter(e=>{
